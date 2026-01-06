b87a1745002b364491120b4f3a209457ce8c67f5
Prince Gupta <beatslevelone@gmail.com>
2026-01-06 21:43:36 +0530

    Update StaticPages.tsx
    Add placeholder exports for static pages

--- apives/pages/StaticPages.tsx
+++ apives/pages/StaticPages.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
-export const TermsPage: React.FC = () => {
-  return null;
-};
+export const TermsPage: React.FC = () => null;
+export const PrivacyPage: React.FC = () => null;
+export const SupportPage: React.FC = () => null;
+export const SponsorshipPage: React.FC = () => null;
