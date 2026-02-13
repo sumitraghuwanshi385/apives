2c2afe535662939ee119551f6101b470c613404f
Prince Gupta <beatslevelone@gmail.com>
2026-02-13 15:19:31 +0530

    Update BuildChatbots.tsx
    fixed

--- apives/pages/build/BuildChatbots.tsx
+++ apives/pages/build/BuildChatbots.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiService } from "../../services/apiClient";
 import { ApiListing } from "../../types";
-import ApiCard from "../../components/ApiCard";
 import { ArrowLeft, ChevronDown, Check } from "lucide-react";
 
 const STORAGE_KEY = "apives_usecase_chatbots";
