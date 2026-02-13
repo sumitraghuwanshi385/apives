131a4406079f8f89c0d8e7b20f307aa5dfa68eaf
Prince Gupta <beatslevelone@gmail.com>
2026-02-13 14:54:36 +0530

    Update App.tsx
    fixed

--- apives/App.tsx
+++ apives/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 
+import BuildChatbots from "./pages/build/BuildChatbots";
 import SponsorAnalytics from "./pages/SponsorAnalytics";
 
 import { Navbar } from './components/Navbar';
@@ -18,69 +19,74 @@ import { PopularApis } from './pages/PopularApis';
 import { OnboardingPage } from './pages/Onboarding';
 
 import {
-  EnterprisePage,
-  DocumentationPage,
-  StatusPage,
-  CookiesPage,
-  PrivacyPage,
-  TermsPage,
-  SupportPage,
-  SponsorshipPage
+EnterprisePage,
+DocumentationPage,
+StatusPage,
+CookiesPage,
+PrivacyPage,
+TermsPage,
+SupportPage,
+SponsorshipPage
 } from './pages/StaticPages';
 
 /* 🔥 Scroll to top on route change */
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+const { pathname } = useLocation();
 
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+React.useEffect(() => {
+window.scrollTo(0, 0);
+}, [pathname]);
 
-  return null;
+return null;
 };
 
 function App() {
-  return (
-    <>
-      <CookieBanner />
-      <ScrollToTop />
+return (
+<>
+<CookieBanner />
+<ScrollToTop />
 
-      <div className="flex flex-col min-h-screen bg-black font-sans text-slate-50 selection:bg-mora-500 selection:text-black">
-        <Navbar />
+<div className="flex flex-col min-h-screen bg-black font-sans text-slate-50 selection:bg-mora-500 selection:text-black">  
+    <Navbar />  
 
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/browse" element={<BrowseApis />} />
-            <Route path="/submit" element={<SubmitApi />} />
-            <Route path="/api/:id" element={<ApiDetails />} />
-            <Route path="/provider" element={<ProviderDashboard />} />
-            <Route path="/access" element={<AccessPage />} />
-            <Route path="/fresh" element={<FreshApis />} />
-            <Route path="/popular" element={<PopularApis />} />
-            <Route path="/onboarding" element={<OnboardingPage />} />
+    <main className="flex-grow">  
+      <Routes>  
+        <Route path="/" element={<LandingPage />} />  
+        <Route path="/browse" element={<BrowseApis />} />  
 
-            {/* Footer */}
-            <Route path="/enterprise" element={<EnterprisePage />} />
-            <Route path="/docs" element={<DocumentationPage />} />
-            <Route path="/status" element={<StatusPage />} />
-            <Route path="/cookies" element={<CookiesPage />} />
-            <Route path="/privacy" element={<PrivacyPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/support" element={<SupportPage />} />
-            <Route path="/sponsorship" element={<SponsorshipPage />} />
+        {/* 🔥 BUILD */}  
+        <Route path="/build/chatbots" element={<BuildChatbots />} />  
 
-            {/* Admin */}
-            <Route path="/admin/sponsors" element={<SponsorAnalytics />} />
+        <Route path="/submit" element={<SubmitApi />} />  
+        <Route path="/api/:id" element={<ApiDetails />} />  
+        <Route path="/provider" element={<ProviderDashboard />} />  
+        <Route path="/access" element={<AccessPage />} />  
+        <Route path="/fresh" element={<FreshApis />} />  
+        <Route path="/popular" element={<PopularApis />} />  
+        <Route path="/onboarding" element={<OnboardingPage />} />  
 
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </main>
+        {/* Footer */}  
+        <Route path="/enterprise" element={<EnterprisePage />} />  
+        <Route path="/docs" element={<DocumentationPage />} />  
+        <Route path="/status" element={<StatusPage />} />  
+        <Route path="/cookies" element={<CookiesPage />} />  
+        <Route path="/privacy" element={<PrivacyPage />} />  
+        <Route path="/terms" element={<TermsPage />} />  
+        <Route path="/support" element={<SupportPage />} />  
+        <Route path="/sponsorship" element={<SponsorshipPage />} />  
 
-        <Footer />
-      </div>
-    </>
-  );
+        {/* Admin */}  
+        <Route path="/admin/sponsors" element={<SponsorAnalytics />} />  
+
+        <Route path="*" element={<Navigate to="/" replace />} />  
+      </Routes>  
+    </main>  
+
+    <Footer />  
+  </div>  
+</>
+
+);
 }
 
 export default App;
