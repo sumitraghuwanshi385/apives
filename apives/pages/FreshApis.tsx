1b7699c9b2f1a5a7fabf430574d21aae13457d96
Prince Gupta <beatslevelone@gmail.com>
2026-02-15 14:50:52 +0530

    Update FreshApis.tsx
    fixed

--- apives/pages/FreshApis.tsx
+++ apives/pages/FreshApis.tsx
@@ -14,7 +14,7 @@ import {
 import { Link, useNavigate } from 'react-router-dom';
 import { ApiListing } from '../types';
 import { Skeleton } from '../components/Skeleton';
-import { ApiCard } from '../components/ApiCard';
+import ApiCard from '../components/ApiCard';
 let API_CACHE: ApiListing[] | null = null;
 
 const CATEGORIES = [
