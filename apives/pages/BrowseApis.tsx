e204e3e39a452c04d9a184caec854582832b65d1
Prince Gupta <beatslevelone@gmail.com>
2026-02-15 17:25:04 +0530

    Update BrowseApis.tsx
    fixed

--- apives/pages/BrowseApis.tsx
+++ apives/pages/BrowseApis.tsx
@@ -241,7 +241,7 @@ export const BrowseApis: React.FC = () => {
         ) : filteredApis.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 mb-12 md:mb-16">
             {filteredApis.map((api) => (
-              <BrowseApiCard
+              <ApiCard
                 key={api._id}
                 api={api}
                 topIds={topIds}
