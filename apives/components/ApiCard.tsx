c36ee67ce63d7472f87cf5b7e4249f10690100b1
Prince Gupta <beatslevelone@gmail.com>
2026-02-16 22:16:55 +0530

    Update ApiCard.tsx
    fixed

--- apives/components/ApiCard.tsx
+++ apives/components/ApiCard.tsx
@@ -355,6 +355,13 @@ const isAdminUser = user?.email === "beatslevelone@gmail.com";
       </div>
 
      <div className="relative pt-4 flex items-center justify-between">
+<div className="absolute top-0 left-0 w-full h-[1px] 
+  bg-gradient-to-r 
+  from-mora-500/10 
+  via-mora-400/60 
+  to-mora-500/10 
+  shadow-[0_0_6px_rgba(16,185,129,0.25)]">
+</div>
 
   
   <div className="flex gap-4 md:gap-6">
