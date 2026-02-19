06a87f15752ea2ce42d0aeb6da3200b6cf89cceb
Prince Gupta <beatslevelone@gmail.com>
2026-02-19 23:08:01 +0530

    Update BuildImageGeneration.tsx
    fixed

--- apives/pages/build/BuildImageGeneration.tsx
+++ apives/pages/build/BuildImageGeneration.tsx
@@ -403,7 +403,7 @@ useEffect(() => {
         </div>
       ) : (
         <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-24">
-          {allApis.map(api => (
+          {curatedApis.map(api => (
             <ApiCard key={api.id} api={api} topIds={[]} />
           ))}
         </div>
