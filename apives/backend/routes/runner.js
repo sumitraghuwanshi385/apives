f5fc9bba57578a3f6419d631c3082f241c7f2b60
Prince Gupta <beatslevelone@gmail.com>
2026-03-11 19:10:48 +0530

    Update runner.js

--- apives/backend/routes/runner.js
+++ apives/backend/routes/runner.js
@@ -4,16 +4,25 @@ const axios = require("axios");
 
 router.post("/run", async (req, res) => {
 
-  const { url, method = "GET", headers = {}, body = null } = req.body;
+  let { url, method, headers, body } = req.body;
 
   if (!url) {
     return res.status(400).json({
-      success: false,
-      error: "URL required"
+      success:false,
+      error:"URL required"
     });
   }
 
   try {
+
+    // convert headers string → JSON
+    if(headers && typeof headers === "string"){
+      try{
+        headers = JSON.parse(headers);
+      }catch{
+        headers = {};
+      }
+    }
 
     const start = Date.now();
 
@@ -21,24 +30,23 @@ router.post("/run", async (req, res) => {
       url,
       method,
       headers,
-      data: body,
-      timeout: 15000
+      data: body
     });
 
-    const time = Date.now() - start;
+    const end = Date.now();
 
     res.json({
-      success: true,
-      status: response.status,
-      time,
-      data: response.data
+      success:true,
+      status:response.status,
+      time:end-start,
+      data:response.data
     });
 
-  } catch (err) {
+  } catch(err){
 
-    res.status(500).json({
-      success: false,
-      error: err.message
+    res.json({
+      success:false,
+      error:err.message
     });
 
   }
