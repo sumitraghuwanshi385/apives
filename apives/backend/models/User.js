6bdba74c153cc5d6e91f0cd807e52b739dd231d7
Prince Gupta <beatslevelone@gmail.com>
2026-03-25 13:25:01 +0530

    Update User.js

--- apives/backend/models/User.js
+++ apives/backend/models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+isVerified: { type: Boolean, default: false },
   credits: { type: Number, default: 100 },
   apiKeys: [String],
   subscriptions: [String],
