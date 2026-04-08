8a0fbad8ec5e95ab54cfd6dca9b74049ffc9a226
Prince Gupta <beatslevelone@gmail.com>
2026-04-08 23:15:18 +0530

    Update server.js

--- apives/backend/server.js
+++ apives/backend/server.js
@@ -173,8 +173,8 @@ mongoose
   });
 
 // ================= SERVER =================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
