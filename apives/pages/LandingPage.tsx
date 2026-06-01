13acd8476260245997acdbecf7656bb3cf19a3ba
Prince Gupta <beatslevelone@gmail.com>
2026-06-01 11:53:49 +0530

    Update LandingPage.tsx

--- apives/pages/LandingPage.tsx
+++ apives/pages/LandingPage.tsx
@@ -23,7 +23,7 @@ import { ApiListing } from '../types';
 import { apiService } from '../services/apiClient';
 import { ApivesAIHighlight } from '../components/ApivesAIHighlight';
 import LiveApiRunner from "../components/LiveApiRunner";
-import LandingApiCard from '../components/LandingApiCard';
+import ApiCard from '../components/ApiCard';
 import SponsorsSection from "../components/SponsorsSection";
 
 let LANDING_API_CACHE:
