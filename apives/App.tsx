b213f2d1c8a45e84061c86998d23baeab6cf5933
Prince Gupta <beatslevelone@gmail.com>
2026-08-07 18:24:27 +0530

    Update App.tsx

--- apives/App.tsx
+++ apives/App.tsx
@@ -12,7 +12,6 @@ import JwtDecoderPage from "./pages/JwtDecoderPage";
 import ApiResponseFormatterPage from "./pages/ApiResponseFormatterPage";
 import CurlConverterPage from "./pages/CurlConverterPage";
 import MockServerPage from "./pages/MockServerPage";
-import ApivesArchitect from "./pages/ApivesArchitect";
 
 
 import { Navbar } from './components/Navbar';
@@ -93,7 +92,6 @@ element={<LiveApiRunnerPage />} />
   element={<CurlConverterPage />} />
 <Route path="/mock-server"
   element={<MockServerPage />} />
-<Route path="/architect" element={<ApivesArchitect />} />
 
 
 
