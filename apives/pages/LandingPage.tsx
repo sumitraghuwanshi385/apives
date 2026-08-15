3009af2fd1c3862db1dd41bc77180636f813fdb4
Prince Gupta <beatslevelone@gmail.com>
2026-08-15 15:17:48 +0530

    Update LandingPage.tsx

--- apives/pages/LandingPage.tsx
+++ apives/pages/LandingPage.tsx
@@ -485,7 +485,7 @@ export const LandingPage: React.FC = () => {
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 mb-12 md:mb-20">
               {universalApis.map((api, idx) => (
-                <ApiCard
+                <LandingApiCard
                   key={`${api.id}-${idx}`}
                   api={api}
                   topIds={top3Ids}
@@ -521,7 +521,7 @@ export const LandingPage: React.FC = () => {
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 mb-12 md:mb-20">
               {freshApis.map((api, idx) => (
-                <ApiCard
+                <LandingApiCard
                   key={`new-${api.id}`}
                   api={api}
                   topIds={top3Ids}
@@ -557,7 +557,7 @@ export const LandingPage: React.FC = () => {
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 mb-12 md:mb-20">
               {communityApis.map((api, idx) => (
-                <ApiCard
+                <LandingApiCard
                   key={`loved-${api.id}`}
                   api={api}
                   topIds={top3Ids}
